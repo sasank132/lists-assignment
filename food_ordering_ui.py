ed543fc6b39301846553c26a75488419b5a80a4c
sasank132 <syerrabothu@murraystate.edu>
2024-09-21 00:11:52 -0500

    sasank yerrabothu third commit

--- food_ordering_ui.py
+++ food_ordering_ui.py
@@ -8,6 +8,7 @@ def show_main_menu():
     print('N for a new order')
     print("C to change the current order")
     print('X for close orders and print the check')
+    print('R to reset order')
     print('Q for quit')
     user_menu_choice = input('Your choice: ')
     if user_menu_choice in 'Qq':
@@ -22,7 +23,10 @@ def show_main_menu():
       while input('Add a dish? y/n: ') in 'Yy':
         ordered_item = functions.get_item_number()
         current_order.append(ordered_item)
-      print('Current order', current_order)  
+      print('Current order', current_order) 
+    elif user_menu_choice in 'Rr':
+       current_order=[]
+       print('Order is reset') 
     else:
       make_order(user_menu_choice.upper())  #calls a function for adding to the orders
 
